530a968bf1e4dd3c55763e59d4a28f813aec5982
Daniel <43097521+Hpro1@users.noreply.github.com>
2022-06-07 18:34:00 +0300

    Week1 done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+document.getElementById("add-comment").onclick = function () {
+  var text = document.getElementById("input").value; //tekstikenttä
+  var li = "<li>" + text + "</li>";
+  document.getElementById("list").insertAdjacentHTML("beforeend", li);
+  document.getElementById("input").value = "";
+};
+
+document.getElementById("remove-comments").onclick = function () {
+  document.getElementById("list").innerHTML = "";
+  window.confirm("Are you sure you want to delete all the comments?");
+};
